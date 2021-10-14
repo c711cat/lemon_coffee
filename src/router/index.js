92aebb04a0056737f5e4e07c4672bd527b78c596
c711cat <c711cat@gmail.com>
2021-10-14 15:14:23 +0800

    豆單元件位置原在 views 中，改放到 components 中

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    component: () => import("@/views/BeanList.vue"),
+    component: () => import("@/components/BeanList.vue"),
   },
   {
     path: "/about",
