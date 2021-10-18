cac3579aae9fe748ae62e9f165abfd8fbbd292a2
c711cat <c711cat@gmail.com>
2021-10-18 16:04:37 +0800

    安裝semantic-ui套件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "semantic-ui-css/semantic.min.css";
 
 const app = createApp(App);
 app.use(router);
