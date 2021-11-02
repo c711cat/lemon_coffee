41d95de22c86613bcc87ca7daab65c744b77b80a
c711cat <c711cat@gmail.com>
2021-11-02 16:49:35 +0800

    1. 刪除沒用到的套件。2. 調整版面舒適度。 3. 刪除console.log。

--- src/components/CleanBeanList.vue
+++ src/components/CleanBeanList.vue
@@ -63,6 +63,7 @@
   </div>
   <DelModal ref="del" :delItem="delItem" />
 </template>
+
 <script>
 import Roast from "@/components/Roast.vue";
 import Draggable from "vuedraggable";
@@ -90,38 +91,44 @@ export default {
   methods: {
     openDelModal(element) {
       this.delItem = element;
-      console.log(this.delItem);
       this.$refs.del.openModal();
     },
   },
 };
 </script>
+
 <style lang="scss" scoped>
 * {
   box-sizing: border-box;
 }
+
 a {
   text-decoration: none;
   color: #333;
 }
+
 .bean-list-unit:hover {
   background: #f9f5ef;
 }
+
 .bean-list-unit:hover .bean-name {
   color: #4aa378;
 }
+
 .wrap {
   max-width: 1200px;
   margin: 0 auto;
   font-family: system-ui;
   padding: 2% 0%;
 }
+
 .bean-list-unit {
   padding: 15px 20px;
   margin: 0 auto;
   text-align: left;
   display: flex;
 }
+
 .unit-up {
   display: flex;
   justify-content: space-between;
@@ -129,44 +136,54 @@ a {
   align-items: center;
   border-bottom: solid 1px black;
 }
+
 .bean-name {
   min-width: 50%;
   font-size: 18px;
   line-height: 25px;
   padding-bottom: 10px;
 }
+
 .bean-price-type {
   display: flex;
   padding-bottom: 10px;
 }
+
 .price-title {
   color: #4aa378;
   margin-right: 5px;
 }
+
 .price-num {
   width: 56px;
   display: inline-block;
 }
+
 .unit-down {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 }
+
 .flavor-container {
   display: flex;
   padding-top: 10px;
 }
+
 .bean-list-unit-left {
   width: 100%;
   margin-right: 2%;
 }
+
 .bean-list-unit-right {
   display: flex;
   margin-top: 2%;
 }
+
 .btn-container {
   display: flex;
 }
+
 .btn-body {
   width: 45px;
   height: 30px;
@@ -175,18 +192,22 @@ a {
   border-radius: 3px;
   cursor: pointer;
 }
+
 .edit {
   color: #aaa;
   border: 1px solid #aaa;
 }
+
 .edit:hover {
   background: #aaa;
   color: #fff;
 }
+
 .del {
   color: rgb(235, 72, 72);
   border: 1.5px solid rgb(235, 72, 72);
 }
+
 .del:hover {
   background: rgb(235, 72, 72);
   color: #fff;
