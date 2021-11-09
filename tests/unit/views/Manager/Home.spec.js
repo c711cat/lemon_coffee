d147d67831cab3608d6b0984784f67bb7232d320
c711cat <c711cat@gmail.com>
2021-11-09 11:09:13 +0800

    測試管理者首頁 : 編輯 及 刪除 的按鈕會在畫面上
    刪除不必要的檔案

--- tests/unit/views/Manager/Home.spec.js
+++ tests/unit/views/Manager/Home.spec.js
@@ -1,10 +1,39 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import Home from "@/views/Manager/Home.vue";
+import axios from "axios";
+
+jest.mock("axios");
+
+const products = [
+  {
+    id: 1,
+    order: 1,
+    name: "耶家雪菲 日曬 古吉 夏奇索 魔魔拉單一莊園 G1",
+    half_pound_price: 450,
+    one_pound_price: 810,
+    drip_bag_price: 40,
+    roast: 1,
+    flavor: ["藍莓", "柑橘", "花香"],
+  },
+];
 
 describe("Manager/Home", () => {
   it("新增產品的按鈕會在畫面上", async () => {
+    axios.get.mockResolvedValue({ data: products });
     const wrapper = shallowMount(Home);
     const btn = wrapper.get(".add-btn");
     expect(btn.text()).toBe("新增產品");
+  });
+
+  it("編輯按鈕會出現在畫面上", async () => {
+    axios.get.mockResolvedValue({ data: products });
+    const wrapper = await mount(Home);
+    expect(wrapper.get(".edit").text()).toBe("編輯");
+  });
+
+  it("刪除按鈕會出現在畫面上", async () => {
+    axios.get.mockResolvedValue({ data: products });
+    const wrapper = await mount(Home);
+    expect(wrapper.get(".del").text()).toBe("刪除");
   });
 });
