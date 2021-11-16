054bc302319ecccca90025873c6549f5c51b84f3
c711cat <c711cat@gmail.com>
2021-11-16 17:14:40 +0800

    更換成 prime-vue UI套件

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -1,105 +1,69 @@
 <template>
-  <div class="add-product-wrap ui form">
-    <h4 class="ui dividing header" v-if="editItem">編輯產品</h4>
-    <h4 class="ui dividing header" v-else>新增產品</h4>
-    <div class="fields">
-      <div class="nine wide field field-content">
-        <label>產品名稱</label>
-        <input
-          class="input-body"
-          type="text"
-          placeholder="請輸入產品名稱"
-          v-model="product.name"
-        />
-      </div>
-      <div class="three wide field field-content">
-        <label for="roast">烘焙度</label>
-        <select id="roast" v-model="product.roast">
-          <option value="" disabled>請選擇烘焙度</option>
-          <option :value="item" v-for="item in [1, 2, 3, 4, 5]" :key="item">
-            {{ item }}
-          </option>
-        </select>
-      </div>
+  <h3>新增產品</h3>
+  <div class="p-fluid p-formgrid p-grid">
+    <div class="p-field p-col-12 p-md-8">
+      <label>產品名稱</label>
+      <InputText type="text" v-model="product.name" />
     </div>
-    <div class="fields">
-      <div class="four wide field field-content">
-        <label>半磅價格</label>
-        <input
-          type="number"
-          placeholder="請輸入半磅價格"
-          min="0"
-          v-model="product.half_pound_price"
-        />
-      </div>
-      <div class="four wide field field-content">
-        <label>一磅價格</label>
-        <input
-          type="number"
-          placeholder="請輸入一磅價格"
-          min="0"
-          v-model="product.one_pound_price"
-        />
-      </div>
-      <div class="four wide field field-content">
-        <label>耳掛價格</label>
-        <input
-          type="number"
-          placeholder="請輸入耳掛價格"
-          min="0"
-          v-model="product.drip_bag_price"
-        />
-      </div>
+
+    <div class="p-field p-col-12 p-md-4">
+      <label>烘焙度</label>
+      <Dropdown v-model="product.roast" :options="[1, 2, 3, 4, 5]" />
     </div>
-    <div class="fields">
-      <div class="four wide field field-content">
-        <label>風味 1 </label>
-        <input
-          type="text"
-          placeholder="請輸入風味"
-          v-model="product.flavor[0]"
-        />
-      </div>
-      <div class="four wide field field-content">
-        <label>風味 2 </label>
-        <input
-          type="text"
-          placeholder="請輸入風味"
-          v-model="product.flavor[1]"
-        />
-      </div>
-      <div class="four wide field field-content">
-        <label>風味 3 </label>
-        <input
-          type="text"
-          placeholder="請輸入風味"
-          v-model="product.flavor[2]"
-        />
-      </div>
+
+    <div class="p-field p-col-12 p-md-4">
+      <label>半磅價格</label>
+      <InputNumber showButtons :min="0" v-model="product.half_pound_price" />
     </div>
-    <h4 class="ui dividing header"></h4>
-    <div class="btn-container">
-      <button class="ui primary button" @click.prevent="onSubmit(this.product)">
-        送出
-      </button>
+
+    <div class="p-field p-col-12 p-md-4">
+      <label>一磅價格</label>
+      <InputNumber showButtons :min="0" v-model="product.one_pound_price" />
+    </div>
+
+    <div class="p-field p-col-12 p-md-4">
+      <label>耳掛價格</label>
+      <InputNumber showButtons :min="0" v-model="product.drip_bag_price" />
+    </div>
+
+    <div class="p-field p-col-12 p-md-4">
+      <label>風味1</label>
+      <InputText type="text" v-model="product.flavor[0]" />
+    </div>
+    <div class="p-field p-col-12 p-md-4">
+      <label>風味2</label>
+      <InputText type="text" v-model="product.flavor[1]" />
+    </div>
+    <div class="p-field p-col-12 p-md-4">
+      <label>風味3</label>
+      <InputText type="text" v-model="product.flavor[2]" />
+    </div>
+
+    <div class="p-field p-col-12 p-md-2 p-md-offset-10">
+      <Button label="送出" @click="onSubmit(this.product)" />
     </div>
   </div>
 </template>
 
 <script>
+import InputText from "primevue/inputtext";
+import Dropdown from "primevue/dropdown";
+import InputNumber from "primevue/inputnumber";
+import Button from "primevue/button";
 export default {
   data() {
     return {
       product: {
         name: "",
-        half_pound_price: "",
-        one_pound_price: "",
-        drip_bag_price: "",
+        half_pound_price: 0,
+        one_pound_price: 0,
+        drip_bag_price: 0,
         roast: "",
         flavor: [],
       },
     };
   },
+  components: { InputText, Dropdown, InputNumber, Button },
   props: {
     editItem: {
       typeof: Object,
@@ -121,29 +85,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.add-product-wrap {
-  max-width: 1200px;
-  margin: 50px auto;
-  padding: 0 25px;
+.p-fluid.p-formgrid.p-grid {
+  max-width: 1000px;
+  margin: 30px auto;
   text-align: left;
-  letter-spacing: 1.3px;
 }
-
-.fields {
-  justify-content: center;
-}
-
-.dividing.header {
-  margin-bottom: 30px;
-}
-
-.btn-container {
-  display: flex;
-  flex-direction: row-reverse;
-  margin-top: -15px;
-}
-
-.wide.field.field-content {
-  margin-bottom: 20px;
+.p-button {
+  margin-top: 20px;
 }
 </style>
