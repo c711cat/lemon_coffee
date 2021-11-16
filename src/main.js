14d9b9c3b0a769d5086278f9ccbd3d07bb8e42be
c711cat <c711cat@gmail.com>
2021-11-16 10:32:53 +0800

    改安裝 prime-vue 2.0版本，刪除 semantic import

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "semantic-ui-css/semantic.min.css";
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 const app = createApp(App);
 app.use(router);
