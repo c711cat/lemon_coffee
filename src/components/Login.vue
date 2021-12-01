7b7b6f5c053b2b53a71571851f0657c92155e487
c711cat <c711cat@gmail.com>
2021-12-01 21:30:07 +0800

    串接登入api

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,13 +2,17 @@
   <div class="p-fluid">
     <div class="p-field">
       <label for="email">Email</label>
-      <InputText id="email" type="text" v-model="email" />
+      <InputText id="email" type="text" v-model="login_data.user.email" />
     </div>
     <div class="p-field">
       <label for="password">密碼</label>
-      <InputText id="password" type="password" v-model="password" />
+      <InputText
+        id="password"
+        type="password"
+        v-model="login_data.user.password"
+      />
     </div>
-    <Button label="登入" />
+    <Button label="登入" @click.prevent="login" />
   </div>
   <div class="p-field a-container">
     <a href="#">忘記密碼</a>
@@ -16,12 +20,26 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      email: "",
-      password: "",
+      login_data: {
+        user: {
+          email: "",
+          password: "",
+        },
+      },
     };
+  },
+  methods: {
+    login() {
+      console.log(this.login_data);
+      const api = `${process.env.VUE_APP_API}/users/sign_in`;
+      axios.post(api, this.login_data).then((response) => {
+        console.log(response);
+      });
+    },
   },
 };
 </script>
