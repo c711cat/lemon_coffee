b91af1b4e9b1cbd6e99405542035242db39dc575
c711cat <c711cat@gmail.com>
2021-12-10 16:32:42 +0800

    最外層根元件放 provide emitter，之後內層皆用 inject emitter

--- src/views/UserRoot.vue
+++ src/views/UserRoot.vue
@@ -5,11 +5,17 @@
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import emitter from "@/methods/emitter.js";
 
 export default {
   data() {
     return {};
   },
   components: { Navbar },
+  provide() {
+    return {
+      emitter,
+    };
+  },
 };
 </script>
