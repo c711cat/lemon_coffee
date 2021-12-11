6e4f367d3e9b1832d571a74de8500ee0b8e8e8c1
c711cat <c711cat@gmail.com>
2021-12-11 11:34:43 +0800

    將小購車元件 - Sidebar 移到根元件中

--- src/views/UserRoot.vue
+++ src/views/UserRoot.vue
@@ -1,21 +1,40 @@
 <template>
-  <Navbar />
+  <Navbar @change-visible="visibleRight = true" />
+  <Sidebar
+    v-model:visible="visibleRight"
+    position="right"
+    class="sidebar-wrap"
+    style="width: 380px"
+  >
+    <CartSidebar></CartSidebar>
+  </Sidebar>
   <router-view />
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
 import emitter from "@/methods/emitter.js";
+import CartSidebar from "@/components/CartSidebar.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      visibleRight: false,
+    };
   },
-  components: { Navbar },
+  components: { Navbar, CartSidebar },
+
   provide() {
     return {
       emitter,
     };
   },
+  
 };
 </script>
+
+<style lang="scss" scoped>
+.sidebar-wrap {
+  width: 500px;
+}
+</style>
