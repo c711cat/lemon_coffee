446b199e3129a226b7b261a5e53e7ac95cb3a68b
c711cat <c711cat@gmail.com>
2021-12-13 16:08:13 +0800

    串接 將商品加入購物車 API

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -18,7 +18,11 @@
       />
     </div>
     <div class="p-col-12">
-      <Button label="加入購物車" class="p-button-info p-col-12 p-lg-4"></Button>
+      <Button
+        label="加入購物車"
+        class="p-button-info p-col-12 p-lg-4"
+        @click.prevent="addToCart"
+      ></Button>
     </div>
   </div>
   <div v-if="is_error">無此商品</div>
@@ -26,17 +30,20 @@
 
 <script>
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export default {
   data() {
     return {
       product: {
         name: "",
+        id: 0,
       },
       typeOfOptions: ["耳掛", "半磅", "一磅"],
       qty: 1,
       type: "半磅",
       is_error: false,
+      typeData: "",
     };
   },
   methods: {
@@ -49,6 +56,39 @@ export default {
         })
         .catch(() => {
           this.is_error = !this.is_error;
+        });
+    },
+    addToCart() {
+      if (this.type === "半磅") {
+        this.typeData = "half_pound";
+      }
+      if (this.type === "一磅") {
+        this.typeData = "one_pound";
+      }
+      if (this.type === "耳掛") {
+        this.typeData = "drip_bag";
+      }
+      const cart = {
+        product_id: this.product.id,
+        package_type: this.typeData,
+        quantity: this.qty,
+      };
+      const api = `${process.env.VUE_APP_API}/users/cart_items`;
+      const headers = { Authorization: Cookies.get("lemonToken") };
+      axios
+        .post(api, { cart_item: cart }, { headers })
+        .then((response) => {
+          if (response.status === 200) {
+            this.$toast.add({
+              severity: "success",
+              summary: "已加入購物車",
+              life: 2000,
+            });
+            history.back();
+          }
+        })
+        .catch(() => {
+          this.$router.push("/entrance/login");
         });
     },
   },
