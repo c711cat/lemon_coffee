be65ae4431d85daa33b54b4228683411b99d8509
c711cat <c711cat@gmail.com>
2021-12-15 16:50:19 +0800

    刪除console.log

--- src/components/CartSidebar.vue
+++ src/components/CartSidebar.vue
@@ -199,7 +199,6 @@ export default {
           }
         })
         .catch((error) => {
-          console.log(error.response);
           if (error.response.data === "Invalid segment encoding") {
             this.$toast.add({
               severity: "error",
