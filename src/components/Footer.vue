c879bfe178d85dbe9c4a522e472a3ee3e9fe501d
c711cat <c711cat@gmail.com>
2021-12-29 13:43:20 +0800

    刪除不需要的部分

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -73,41 +73,10 @@
         </div>
       </div>
     </div>
-
-    <!-- <div class="p-col-12 p-lg-3 p-text-center p-jc-center p-ai-center">
-      <div class="p-col-12 eng-text">TUE－SUN 10:00－22:00</div>
-
-      <a
-        style="display: block"
-        class="link-content logo-hover p-col-12"
-        href="https://www.google.com/maps/place/%E5%AF%A7%E8%92%99%E5%92%96%E5%95%A1/@22.9268643,120.1866136,15z/data=!4m5!3m4!1s0x0:0x823a5a6d873727a5!8m2!3d22.9268642!4d120.1866136"
-        >台南市南區明興路1151巷10-2號
-      </a>
-
-      <div class="p-col-12 p-pt-2">0983-732-995</div>
-    </div>
-
-    <a
-      style="display: block"
-      class="link-content"
-      href="https://www.facebook.com/LemonCafes/"
-    >
-      <Button
-        class="
-          p-button-rounded p-button-text p-button-plain p-col-12 p-jc-center
-        "
-      >
-        <i class="pi pi-facebook" style="font-size: 2rem"></i>
-      </Button>
-    </a> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
-* {
-  border: 1px solid black;
-}
-
 .footer-body {
   background: #f8f9fa;
   color: #495057;
