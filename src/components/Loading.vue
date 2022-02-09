11887e37ef7b3fd31d8a7e15a456db3c361f3dc5
c711cat <c711cat@gmail.com>
2022-02-09 15:20:16 +0800

    更改顯示的文字內容

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -2,7 +2,7 @@
   <div style="height: 820px" class="p-d-flex p-jc-center p-ai-center">
     <div>
       <ProgressSpinner fill="var(--surface-ground)"></ProgressSpinner>
-      <div class="p-text-center">Loading...</div>
+      <div class="p-text-center">驗證中...</div>
     </div>
   </div>
 </template>
