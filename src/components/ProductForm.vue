505b36e1e539f69707c9db0b0979e7a1da7bc515
c711cat <c711cat@gmail.com>
2022-02-21 17:48:31 +0800

    加入產區、品種、處理、風味描述的 UI ，若無選單內品項也可以自行手動輸入。

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -40,7 +40,7 @@
       <InputText type="text" v-model="product.flavor[2]" />
     </div>
 
-    <div class="p-field p-col-12 p-md-4">
+    <div class="p-field p-col-12 p-md-3">
       <label>國家</label>
       <Dropdown
         v-model="product.country"
@@ -48,6 +48,41 @@
         optionLabel="name"
         :editable="true"
       />
+    </div>
+
+    <div class="p-field p-col-12 p-md-3">
+      <label>產區</label>
+      <Dropdown
+        v-model="product.area"
+        :options="areas"
+        optionLabel="name"
+        :editable="true"
+      />
+    </div>
+
+    <div class="p-field p-col-12 p-md-3">
+      <label>品種</label>
+      <Dropdown
+        v-model="product.variety"
+        :options="varieties"
+        optionLabel="name"
+        :editable="true"
+      />
+    </div>
+
+    <div class="p-field p-col-12 p-md-3">
+      <label>處理法</label>
+      <Dropdown
+        v-model="product.approach"
+        :options="approaches"
+        optionLabel="name"
+        :editable="true"
+      />
+    </div>
+
+    <div class="p-field p-col-12 p-md-12">
+      <label>風味描述</label>
+      <InputText type="text" v-model="product.discription" />
     </div>
 
     <div class="p-field p-col-12 p-md-2 p-md-offset-10">
@@ -68,6 +103,10 @@ export default {
         roast: "",
         flavor: [],
         country: "",
+        area: "",
+        variety: "",
+        approach: "",
+        discription: "",
       },
       countries: [
         { name: "印尼" },
@@ -81,6 +120,45 @@ export default {
         { name: "哥倫比亞" },
         { name: "哥斯大黎加" },
         { name: "巴布亞新幾內亞" },
+        { name: "手動填入" },
+      ],
+      areas: [
+        { name: "肯亞" },
+        { name: "林東省" },
+        { name: "龐貝村" },
+        { name: "耶加雪菲" },
+        { name: "天堂產區" },
+        { name: "聖荷西莊園" },
+        { name: "遊樂園莊園" },
+        { name: "奇邁爾莊園" },
+        { name: "格比亞多莊園" },
+        { name: "阿拉斯加莊園" },
+        { name: "聖塔費麗莎莊園" },
+        { name: "魔魔拉單一莊園" },
+        { name: "山塔那小農協會" },
+        { name: "手動填入" },
+      ],
+      varieties: [
+        { name: "波旁種" },
+        { name: "橘波旁" },
+        { name: "黃波旁" },
+        { name: "鐵比卡" },
+        { name: "卡度艾" },
+        { name: "原生種" },
+        { name: "阿拉比卡" },
+        { name: "混合品種" },
+        { name: "帕拉伊內瑪種" },
+        { name: "SL 28" },
+        { name: "手動填入" },
+      ],
+
+      approaches: [
+        { name: "水洗" },
+        { name: "日曬" },
+        { name: "去果皮日曬" },
+        { name: "蜜處理" },
+        { name: "黃蜜處理" },
+        { name: "濕剝處理" },
         { name: "手動填入" },
       ],
     };
