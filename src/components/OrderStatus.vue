97bc98f3ef9af671e391bff55d302d9e4725fe42
c711cat <c711cat@gmail.com>
2022-03-09 17:25:25 +0800

    加入取消訂單的狀態

--- src/components/OrderStatus.vue
+++ src/components/OrderStatus.vue
@@ -1,25 +1,39 @@
 <template>
-  <strong :style="pending"> 處理中 </strong>
-
-  <i class="pi pi-arrow-right p-mx-1"></i>
-
-  <Button
-    @click="confirm_order"
-    v-if="confirm_order_btn"
-    label="確認訂單"
-    class="p-button-raised p-button-info p-button-sm p-lg-fixed p-col-3 p-px-2"
-    style="width: 100px"
-  />
-  <strong v-if="!confirm_order_btn" :style="confirmed">已確認</strong>
-
-  <i :style="confirmed_arrow" class="pi pi-arrow-right p-mx-1"></i>
-
-  <strong :style="finished">已完成</strong>
-  <i
-    v-if="current_status === 'finished'"
-    class="pi pi-check-circle p-ml-1 success-color"
+  <div
+    v-if="current_status !== 'canceled'"
+    class="p-col-12 p-lg-11 p-d-flex p-jc-start p-ai-center"
   >
-  </i>
+    <strong :style="pending"> 處理中 </strong>
+
+    <i class="pi pi-arrow-right p-mx-1"></i>
+
+    <Button
+      @click="confirm_order"
+      v-if="confirm_order_btn"
+      label="確認訂單"
+      class="
+        p-button-raised p-button-info p-button-sm p-lg-fixed p-col-3 p-px-2
+      "
+      style="width: 100px"
+    />
+    <strong v-if="!confirm_order_btn" :style="confirmed">已確認</strong>
+
+    <i :style="confirmed_arrow" class="pi pi-arrow-right p-mx-1"></i>
+
+    <strong :style="finished">已完成</strong>
+    <i
+      v-if="current_status === 'finished'"
+      class="pi pi-check-circle p-ml-1 success-color"
+    >
+    </i>
+  </div>
+
+  <div
+    v-if="current_status === 'canceled'"
+    class="p-col-12 p-lg-11 p-d-flex p-jc-start p-ai-center"
+  >
+    <strong class="cancel-color"> 已取消 </strong>
+  </div>
 </template>
 
 <script>
@@ -34,6 +48,20 @@ export default {
       finished: "color: #ccc",
     };
   },
+  props: {
+    orderStatus: {
+      type: String,
+      default() {
+        return;
+      },
+    },
+  },
+  watch: {
+    orderStatus() {
+      this.current_status = this.orderStatus;
+      console.log(typeof this.current_status);
+    },
+  },
   methods: {
     confirm_order() {
       this.current_status = "confirmed";
@@ -46,5 +74,9 @@ export default {
 <style lang="scss" scoped>
 .success-color {
   color: #689f38;
+}
+
+.cancel-color {
+  color: #d32f2f;
 }
 </style>
--- src/components/ShippingStatus.vue
+++ src/components/ShippingStatus.vue
@@ -1,43 +1,60 @@
 <template>
-  <strong :style="in_preparation"> 備貨中 </strong>
-
-  <i class="pi pi-arrow-right p-mx-1"></i>
-  <Button
-    @click="confirm_shipped"
-    v-if="shipping_btn"
-    label="確認發貨"
-    class="p-button-raised p-button-info p-button-sm p-lg-fixed p-col-2 p-px-2"
-    style="width: 100px"
-  />
-  <strong v-if="!shipping_btn" :style="shipped"> 已發貨 </strong>
-
-  <i class="pi pi-arrow-right p-mx-1" :style="shipping_arrow"></i>
-
-  <Button
-    @click="confirm_arrived"
-    v-if="arrived_btn"
-    label="確認到達"
-    class="p-button-raised p-button-info p-button-sm p-lg-fixed p-col-2 p-px-2"
-    style="width: 100px"
-  />
-  <strong v-if="!arrived_btn" :style="arrived"> 已到達 </strong>
-
-  <i class="pi pi-arrow-right p-mx-1" :style="arrived_arrow"></i>
-
-  <Button
-    @click="confirm_picked_up"
-    v-if="picked_up_btn"
-    label="確認取貨"
-    class="p-button-raised p-button-info p-button-sm p-lg-fixed p-col-3 p-px-2"
-    style="width: 100px"
-  />
-
-  <strong v-if="!picked_up_btn" :style="picked_up"> 已取貨 </strong>
-  <i
-    v-if="current_status === 'picked_up'"
-    class="pi pi-check-circle success-color p-ml-1"
+  <div
+    v-if="current_status !== 'canceled'"
+    class="p-col-12 p-lg-11 p-d-flex p-jc-start p-ai-center"
   >
-  </i>
+    <strong :style="in_preparation"> 備貨中 </strong>
+
+    <i class="pi pi-arrow-right p-mx-1"></i>
+    <Button
+      @click="confirm_shipped"
+      v-if="shipping_btn"
+      label="確認發貨"
+      class="
+        p-button-raised p-button-info p-button-sm p-lg-fixed p-col-2 p-px-2
+      "
+      style="width: 100px"
+    />
+    <strong v-if="!shipping_btn" :style="shipped"> 已發貨 </strong>
+
+    <i class="pi pi-arrow-right p-mx-1" :style="shipping_arrow"></i>
+
+    <Button
+      @click="confirm_arrived"
+      v-if="arrived_btn"
+      label="確認到達"
+      class="
+        p-button-raised p-button-info p-button-sm p-lg-fixed p-col-2 p-px-2
+      "
+      style="width: 100px"
+    />
+    <strong v-if="!arrived_btn" :style="arrived"> 已到達 </strong>
+
+    <i class="pi pi-arrow-right p-mx-1" :style="arrived_arrow"></i>
+
+    <Button
+      @click="confirm_picked_up"
+      v-if="picked_up_btn"
+      label="確認取貨"
+      class="
+        p-button-raised p-button-info p-button-sm p-lg-fixed p-col-3 p-px-2
+      "
+      style="width: 100px"
+    />
+
+    <strong v-if="!picked_up_btn" :style="picked_up"> 已取貨 </strong>
+    <i
+      v-if="current_status === 'picked_up'"
+      class="pi pi-check-circle success-color p-ml-1"
+    >
+    </i>
+  </div>
+  <div
+    v-if="current_status === 'canceled'"
+    class="p-col-12 p-lg-11 p-d-flex p-jc-start p-ai-center"
+  >
+    <strong class="cancel-color"> 已取消 </strong>
+  </div>
 </template>
 
 <script>
@@ -56,7 +73,20 @@ export default {
       picked_up_btn: false,
     };
   },
-
+  props: {
+    orderStatus: {
+      type: String,
+      default() {
+        return;
+      },
+    },
+  },
+  watch: {
+    orderStatus() {
+      this.current_status = this.orderStatus;
+      console.log(typeof this.current_status);
+    },
+  },
   methods: {
     confirm_shipped() {
       this.current_status = "shipping";
@@ -90,5 +120,9 @@ export default {
 <style lang="scss" scoped>
 .success-color {
   color: #689f38;
+}
+
+.cancel-color {
+  color: #d32f2f;
 }
 </style>
