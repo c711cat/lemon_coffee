7c151474041eb1e6de81f395b1d0861cd0fe1d9d
c711cat <c711cat@gmail.com>
2022-03-11 11:21:55 +0800

    修改物流狀態改變方式，用 :class + computed

--- src/components/ShippingStatus.vue
+++ src/components/ShippingStatus.vue
@@ -6,38 +6,38 @@
     <strong class="cancel-color"> 已取消 </strong>
   </div>
   <div v-else class="p-col-12 p-lg-11 p-d-flex p-jc-start p-ai-center">
-    <strong :style="in_preparation"> 備貨中 </strong>
+    <strong class="progress-color"> 備貨中 </strong>
 
     <i class="pi pi-arrow-right p-mx-1"></i>
     <Button
       @click="confirm_shipped"
-      v-if="shipping_btn"
+      v-if="shippingBtn"
       label="確認發貨"
       class="
         p-button-raised p-button-info p-button-sm p-lg-fixed p-col-2 p-px-2
       "
       style="width: 100px"
     />
-    <strong v-if="!shipping_btn" :style="shipped"> 已發貨 </strong>
+    <strong v-if="!shippingBtn" class="progress-color"> 已發貨 </strong>
 
-    <i class="pi pi-arrow-right p-mx-1" :style="shipping_arrow"></i>
+    <i class="pi pi-arrow-right p-mx-1" :class="shipping_arrow_style"></i>
 
     <Button
       @click="confirm_arrived"
-      v-if="arrived_btn"
+      v-if="arrivedBtn"
       label="確認到達"
       class="
         p-button-raised p-button-info p-button-sm p-lg-fixed p-col-2 p-px-2
       "
       style="width: 100px"
     />
-    <strong v-if="!arrived_btn" :style="arrived"> 已到達 </strong>
+    <strong v-if="!arrivedBtn" :class="arrivedStyle"> 已到達 </strong>
 
-    <i class="pi pi-arrow-right p-mx-1" :style="arrived_arrow"></i>
+    <i class="pi pi-arrow-right p-mx-1" :class="arrived_arrow_style"></i>
 
     <Button
       @click="confirm_picked_up"
-      v-if="picked_up_btn"
+      v-if="pickedUpBtn"
       label="確認取貨"
       class="
         p-button-raised p-button-info p-button-sm p-lg-fixed p-col-3 p-px-2
@@ -45,7 +45,7 @@
       style="width: 100px"
     />
 
-    <strong v-if="!picked_up_btn" :style="picked_up"> 已取貨 </strong>
+    <strong v-if="!pickedUpBtn" :class="picked_up_style"> 已取貨 </strong>
     <i
       v-if="current_status === 'picked_up'"
       class="pi pi-check-circle success-color p-ml-1"
@@ -58,16 +58,7 @@
 export default {
   data() {
     return {
-      current_status: "",
-      in_preparation: "color: #0288d1",
-      shipping_btn: true,
-      shipped: "color: #0288d1",
-      shipping_arrow: "color: #ccc",
-      arrived: "color: #ccc",
-      arrived_btn: false,
-      arrived_arrow: "color: #ccc",
-      picked_up: "color: #ccc",
-      picked_up_btn: false,
+      current_status: "in_preparation",
     };
   },
   props: {
@@ -87,42 +78,113 @@ export default {
   methods: {
     confirm_shipped() {
       this.current_status = "shipping";
-      this.shipping_btn = false;
-      this.arrived_btn = true;
-      this.shipping_arrow = "color: none";
       this.emitter.emit("update_shipping_status", this.current_status);
     },
     confirm_arrived() {
       this.current_status = "arrived";
-      this.shipping_btn = false;
-      this.arrived_btn = false;
-      this.picked_up_btn = true;
-      this.shipping_arrow = "color: none";
-      this.arrived_arrow = "color: none";
-      this.arrived = "color: #0288d1";
       this.emitter.emit("update_shipping_status", this.current_status);
     },
     confirm_picked_up() {
       this.current_status = "picked_up";
-      this.shipping_btn = false;
-      this.arrived_btn = false;
-      this.picked_up_btn = false;
-      this.shipping_arrow = "color: none";
-      this.arrived_arrow = "color: none";
-      this.arrived = "color: #0288d1";
-      this.picked_up = "color: #689f38";
       this.emitter.emit("update_shipping_status", this.current_status);
+    },
+  },
+  computed: {
+    shippingBtn() {
+      let btn = true;
+      if (this.current_status === "in_preparation") {
+        btn = true;
+      } else {
+        btn = false;
+      }
+      return btn;
+    },
+    arrivedBtn() {
+      let btn = false;
+      if (this.current_status === "shipping") {
+        btn = true;
+      } else {
+        btn = false;
+      }
+      return btn;
+    },
+    pickedUpBtn() {
+      let btn = false;
+      if (this.current_status === "arrived") {
+        btn = true;
+      } else {
+        btn = false;
+      }
+      return btn;
+    },
+    arrivedStyle() {
+      let color = "";
+      if (
+        this.current_status === "arrived" ||
+        this.current_status === "picked_up"
+      ) {
+        color = "progress-color";
+      } else {
+        color = "disabled-color";
+      }
+      return color;
+    },
+    shipping_arrow_style() {
+      let color = "";
+      if (
+        this.current_status === "shipping" ||
+        this.current_status === "arrived" ||
+        this.current_status === "picked_up"
+      ) {
+        color = "arrow-color";
+      } else {
+        color = "disabled-color";
+      }
+      return color;
+    },
+    arrived_arrow_style() {
+      let color = "";
+      if (
+        this.current_status === "arrived" ||
+        this.current_status === "picked_up"
+      ) {
+        color = "arrow-color";
+      } else {
+        color = "disabled-color";
+      }
+      return color;
+    },
+    picked_up_style() {
+      let color = "";
+      if (this.current_status === "picked_up") {
+        color = "success-color";
+      } else {
+        color = "disabled-color";
+      }
+      return color;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.disabled-color {
+  color: #ccc;
+}
+
+.progress-color {
+  color: #0288d1;
+}
+
 .success-color {
   color: #689f38;
 }
 
 .cancel-color {
   color: #d32f2f;
+}
+
+.arrow-color {
+  color: none;
 }
 </style>
