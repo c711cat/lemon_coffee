82d24a6e1f6062e46da9b4f575814f033779e94a
c711cat <c711cat@gmail.com>
2022-03-26 23:44:51 +0800

    瀏覽頁的標籤 title 更改

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: '/lemon_coffee/dist/'
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "Lemon Coffee";
+      return args;
+    });
+  },
+  publicPath: "/lemon_coffee/dist/",
 }
