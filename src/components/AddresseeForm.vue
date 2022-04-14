bdd77e5528dc47309ba7281a3725fe29c80b0141
c711cat <c711cat@gmail.com>
2022-04-14 16:13:24 +0800

    增加收件人資料表單有 Vuelidate 功能

--- src/components/AddresseeForm.vue
+++ src/components/AddresseeForm.vue
@@ -3,17 +3,69 @@
     <h4>收件人資料</h4>
   </div>
 
-  <div class="p-grid nested-grid p-m-1 p-pl-2">
+  <form
+    @submit.prevent="toCheckout(!v$.$invalid)"
+    class="p-grid nested-grid p-m-1 p-pl-2"
+  >
     <div class="p-col-12 p-lg-7">
       <div class="p-grid p-fluid p-ai-center">
-        <div class="p-col-4 p-lg-2 p-text-bold">姓名</div>
+        <div
+          class="p-col-4 p-lg-2 p-text-bold"
+          :class="{
+            'p-error': v$.shipping_info.name.$invalid && submitted,
+          }"
+        >
+          姓名
+        </div>
         <div class="p-col-8 p-lg-10">
-          <InputText type="text" v-model="shipping_info.name" />
+          <InputText
+            type="text"
+            v-model="v$.shipping_info.name.$model"
+            :class="{
+              'p-invalid': v$.shipping_info.name.$invalid && submitted,
+            }"
+          />
+          <small
+            v-if="
+              (v$.shipping_info.name.$invalid && submitted) ||
+              v$.shipping_info.name.$pending.$response
+            "
+            class="p-error"
+            >{{
+              v$.shipping_info.name.required.$message.replace("Value", "姓名")
+            }}
+          </small>
         </div>
 
-        <div class="p-col-4 p-lg-2 p-text-bold">電話</div>
+        <div
+          class="p-col-4 p-lg-2 p-text-bold"
+          :class="{
+            'p-error': v$.shipping_info.name.$invalid && submitted,
+          }"
+        >
+          電話
+        </div>
         <div class="p-col-8 p-lg-10">
-          <InputText type="text" v-model="shipping_info.phone_number" />
+          <InputText
+            type="text"
+            v-model="v$.shipping_info.phone_number.$model"
+            :class="{
+              'p-invalid': v$.shipping_info.phone_number.$invalid && submitted,
+            }"
+          />
+          <small
+            v-if="
+              (v$.shipping_info.phone_number.$invalid && submitted) ||
+              v$.shipping_info.phone_number.$pending.$response
+            "
+            class="p-error"
+            >{{
+              v$.shipping_info.phone_number.required.$message.replace(
+                "Value",
+                "電話"
+              )
+            }}
+          </small>
         </div>
 
         <div class="p-col-4 p-lg-2 p-text-bold">Email</div>
@@ -21,32 +73,105 @@
           <InputText type="text" v-model="shipping_info.email" />
         </div>
 
-        <div class="p-col-4 p-lg-2 p-text-bold">送貨方式</div>
+        <div
+          class="p-col-4 p-lg-2 p-text-bold"
+          :class="{
+            'p-error': v$.shipping_info.shipping_method.$invalid && submitted,
+          }"
+        >
+          送貨方式
+        </div>
         <div class="p-col-8 p-lg-10">
           <Dropdown
             @change="emitShippingMethod"
-            v-model="shipping_info.shipping_method"
+            v-model="v$.shipping_info.shipping_method.$model"
             :options="shipping_methods"
             optionLabel="label"
             optionValue="value"
+            :class="{
+              'p-invalid':
+                v$.shipping_info.shipping_method.$invalid && submitted,
+            }"
           />
+          <small
+            v-if="
+              (v$.shipping_info.shipping_method.$invalid && submitted) ||
+              v$.shipping_info.shipping_method.$pending.$response
+            "
+            class="p-error"
+            >{{
+              v$.shipping_info.shipping_method.required.$message.replace(
+                "Value",
+                "送貨方式"
+              )
+            }}
+          </small>
         </div>
 
-        <div v-if="isHomeDelivery" class="p-col-4 p-lg-2 p-text-bold">
+        <div
+          v-if="isHomeDelivery"
+          class="p-col-4 p-lg-2 p-text-bold"
+          :class="{
+            'p-error': v$.shipping_info.address.$invalid && submitted,
+          }"
+        >
           收件地址
         </div>
         <div v-if="isHomeDelivery" class="p-col-8 p-lg-10">
-          <InputText type="text" v-model="shipping_info.address" />
+          <InputText
+            type="text"
+            v-model="v$.shipping_info.address.$model"
+            :class="{
+              'p-invalid': v$.shipping_info.address.$invalid && submitted,
+            }"
+          />
+          <small
+            v-if="
+              (v$.shipping_info.address.$invalid && submitted) ||
+              v$.shipping_info.address.$pending.$response
+            "
+            class="p-error"
+            >{{
+              v$.shipping_info.address.required.$message.replace(
+                "Value",
+                "收件地址"
+              )
+            }}
+          </small>
         </div>
 
-        <div class="p-col-4 p-lg-2 p-text-bold">付款方式</div>
+        <div
+          class="p-col-4 p-lg-2 p-text-bold"
+          :class="{
+            'p-error': v$.shipping_info.payment_method.$invalid && submitted,
+          }"
+        >
+          付款方式
+        </div>
         <div class="p-col-8 p-lg-10">
           <Dropdown
-            v-model="shipping_info.payment_method"
+            v-model="v$.shipping_info.payment_method.$model"
             :options="payment_methods"
             optionLabel="label"
             optionValue="value"
+            :class="{
+              'p-invalid':
+                v$.shipping_info.payment_method.$invalid && submitted,
+            }"
           />
+          <small
+            v-if="
+              (v$.shipping_info.payment_method.$invalid && submitted) ||
+              v$.shipping_info.payment_method.$pending.$response
+            "
+            class="p-error"
+            >{{
+              v$.shipping_info.payment_method.required.$message.replace(
+                "Value",
+                "付款方式"
+              )
+            }}
+          </small>
         </div>
 
         <div class="p-col-4 p-lg-2 p-text-bold">備註</div>
@@ -59,19 +184,26 @@
       class="p-grid p-ai-end p-jc-end p-col-12 p-lg-5 p-pb-3 p-m-0 link-content"
     >
       <Button
-        @click.prevent="toCheckout"
+        type="submit"
         class="p-button-lg p-button-info p-button-raised"
         label="去買單"
       >
       </Button>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@/utils/i18n-validators.js";
+
 export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
+      submitted: false,
       note: "",
       shipping_info: {
         name: "",
@@ -79,9 +211,21 @@ export default {
         address: "",
         email: "",
         shipping_method: "",
+        payment_method: "",
       },
       shipping_methods: [{ label: "宅配", value: "home_delivery" }],
       payment_methods: [{ label: "貨到付款", value: "cash_on_delivery" }],
+    };
+  },
+  validations() {
+    return {
+      shipping_info: {
+        name: { required },
+        phone_number: { required },
+        address: { required },
+        shipping_method: { required },
+        payment_method: { required },
+      },
     };
   },
   inject: ["emitter"],
@@ -94,7 +238,12 @@ export default {
         this.emitShippingMethod();
       }
     },
-    toCheckout() {
+    toCheckout(isFormValid) {
+      this.submitted = true;
+      if (!isFormValid) {
+        return;
+      }
+
       const buyer = { note: this.note, shipping_info: this.shipping_info };
       localStorage.setItem("personalData", JSON.stringify(buyer));
       this.$router.push("/checkout");
